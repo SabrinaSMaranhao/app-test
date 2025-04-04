79f2bd93c8c97da1a148c69204441876f51411ee
Sabrina <sabrina.maranhao@etec.sp.gov.br>
2025-04-03 22:01:06 -0300

    alteracao de botoes

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-import { View, Text } from "react-native";
+import CustomButton from "@/components/Title";
+import React, { useState } from "react";
+import { View, Text, Button } from "react-native";
 
 export default function HomeScreen(){
+    const [count, setCount] = useState(0);
 
     return(
         <View>
-            <Text>Exemplo de Texto</Text>
+            <Text>Bem Vindo ao meu app!</Text>
+            <Text>contador: {count}</Text>
+            <Button title="Aumentar" onPress={() => {setCount(count + 1)}}/>
+            <Button title="Diminuir" onPress={() =>{setCount(count - 1)}}/>
+            <Button title="reiniciar" onPress={() =>{setCount(0)}}/>
+                <CustomButton title="NOME"/>
         </View>
+        
     );
 }
